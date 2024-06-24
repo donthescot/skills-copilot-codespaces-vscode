078486876200377f26b057942421c62670b2909b
Don Clark <97646472+donthescot@users.noreply.github.com>
2024-06-24 03:52:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  var skills = [
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "React",
+    "Node",
+    "Express",
+    "MongoDB",
+    "Python",
+    "Django",
+    "Java",
+    "Spring",
+    "SQL",
+    "Git",
+  ];
+  return skills;
+}
